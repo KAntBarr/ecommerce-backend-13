d92bd4e8cffcbb8e2716dc813e730ea11b867486
Koby <kobysac@gmail.com>
2023-11-07 23:43:00 -0800

    can get all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,16 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll();
+    return res.json(categories);
+  } catch (error) {
+    return res.status(500);
+  }
+
 });
 
 router.get('/:id', (req, res) => {
